63cd658e763ae4231671e2516c74c243b50cb7ce
IanPC <iancaminhas25@hotmail.com>
2023-03-08 20:51:57 -0300

    Tratamento de erros e localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Title, Form, Repos } from './styles';
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/logo.svg';
 //Através da segiunte biblioteca, consigo importar icones do Font Awesome, Material UI, bootstrap, etc... tudo compilado dentro de uma lib: npm install react-icons
 //aqui estão todos os icones do fader icon. Fazendo react-icons/ preciso informar a fonte dos ícones
@@ -20,7 +20,18 @@ interface GithubRepository {
 export const Dashboard: React.FC = () => {
   //é estado inicial da lista de repositórios. Inicialmente recebe um [], ou seja, um array vazio.
   //Prciso informar o tipo que o estado vai manipular. No caso é o GithubRepository[]
-  const [repos, setRepos] = React.useState<GithubRepository[]>([]);
+  //A cada vez que fecho a aplicação, repos não existe mais. Solução: Local Storage
+  const [repos, setRepos] = React.useState<GithubRepository[]>(() => {
+    //Isso aqui é para preencher a lista de repos de cara, ou seja, quando o const Dashboard for carregado
+    //se existe a chave, pego o conteúdo dela e armazeno em storageRepos
+    const storageRepos = localStorage.getItem('@GitCollection:repositories');
+    //se existe algo em storageRepos, faço um parse e coloco em repos
+    if (storageRepos) {
+      return JSON.parse(storageRepos);
+    }
+    //se não existe nada em storageRepos, retorno um array vazio
+    return [];
+  });
 
   //pegar o que foi digitado(username/repository_name), juntar na url e mandar o axios buscar pra mim.
   //Para pegar a informação do formulário, eu também preciso de um estado para lidar com o input. Assim, o campo também precisa de um estado.
@@ -29,12 +40,38 @@ export const Dashboard: React.FC = () => {
   //'' significa que o estado inicial é sem valor(vazio)
   const [newRepo, setNewRepo] = React.useState('');
 
+  //criar um estado para armazenar a mensagem de erro
+  /*À medida que o usuário digitar um repositório que não existe, temos que informar algo:
+  Exemplo: se não digitar nada, preciso exibir uma mensagem de erro solicitando uma username/repositoryname */
+  //Vai compartar uma string dizendo para o usuário o que ele precisa fazer para usar a aplicação
+  const [inputError, setInputError] = React.useState('');
+
   //React.ChangeEvent<HTMLInputElement> -> qual tipo de elemento hmtl esse evento está atrelado ?
   //pego onde o evento gera alteração: ChangeEvent... Alteração em que ? um elemento input HTML -> HTMLInputElement
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
     //Quando esse elemento se modificar, vou pegar o valor alocado e redefinir o estado. a função setNewRepo atualiza o estado newRepo
     setNewRepo(event.target.value);
   }
+
+  /*Execução de função quando o efeito colateral ocorre. useEffect...
+  Quando o componente é montado, posso ter a função do useEfec executada.
+  Também posso configurar esse useEffect para ser chamado em otutro momento. exemplo:
+  Quando o valor de um estado ou variável alterar.
+  Pra que isso ? para salvar a lista de objetos atualizada e trabalhar essa lista junto com o local storage.
+  useEffect é um hook do React
+  A função executada quando o efeito colateral ocorrer. Arrow function.
+  Isso é executado quando o componente é carregado:
+     React.useEffect(()=>{}, [])
+
+  Quando a variável que estiver estiver entre [] for alterada, a função ()=>{} é executada novamente
+  */
+  React.useEffect(() => {
+    //Chave: @GitCollection:repositories. O recomendado é utilizar o nome da aplicação
+    //Valor: a lista de repositórios atualizada
+    localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+    //Toda vez que o estado repos for modificado([repos]), o useEffect vai executar o trecho acima novamente.
+    //A cada vez que repos é atualizada, isso é sobrescrito: '@GitCollection:repositories', JSON.stringify(repos)
+  }, [repos]);
 
   //Com estado alterado, ou seja, com o novo valor adquirido consigo fazer a chamada no axios. Vou pegar o newRepo hehehe
   //O evento onSubmit tenta faz a requisição e um reload da página. Quero evitar isso passando um tipo de evento chamado React.FormEvent<HtmlFormElement>
@@ -43,6 +80,13 @@ export const Dashboard: React.FC = () => {
   ): Promise<void> {
     //não executar a ação padrão do onSubmit de fazer reload
     event.preventDefault();
+
+    //Se não tiver nada em newRepo, informo um erro e paro a aplicação com o return
+    if (!newRepo) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
+
     //Ao fazer o get, o axios já sabe o que vai retornar: await api.get<GithubRepository>
     //Nós informamos a url base, não é necessário informá-la mais: 'https://api.github.com'. Está em services/api.ts
     const response = await api.get<GithubRepository>(`repos/${newRepo}`);
@@ -66,13 +110,15 @@ export const Dashboard: React.FC = () => {
       <img src={logo} alt="GitCollection" />
       <Title>Catálago de repositórios do Github</Title>
 
-      <Form onSubmit={handleAddRepo}>
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
         <input
           placeholder="username/repository_name"
           onChange={handleInputChange} //quando o input for alterado, executar algo nessa função. Qualquer alteração, jogo no newRepo
         />
         <button type="submit">Buscar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Repos>
         {repos.map(repository => (
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 //instância do style-components que foi importada
 //fazendo styled. tenho todas as tags do html
@@ -12,7 +16,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex; //manter o botao ao lado do form
@@ -25,6 +29,12 @@ export const Form = styled.form`
     border-radius: 5px 0px 0px 5px;
     color: #3a3a3a; //cor do texto que vou digitar
     border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3; //deixa o placeholder um pouco mais claro
@@ -101,4 +111,10 @@ export const Repos = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
