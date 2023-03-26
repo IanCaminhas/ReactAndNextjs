77ae0a935060c03d3cd47925352226723c8fae05
IanPC <iancaminhas25@hotmail.com>
2023-03-26 20:23:51 -0300

    Webpack Magic Comments

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 //import { Repo } from '../pages/Repo';
 
 //esse tipo de import exige o export default em cada componente
-const Dashboard = React.lazy(() => import('../pages/Dashboard'));
-const Repo = React.lazy(() => import('../pages/Repo'));
+const Dashboard = React.lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */ /* webpackChunkName: "dashboard" */ '../pages/Dashboard'
+    ),
+);
+const Repo = React.lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */ /* webpackChunkName: "repo" */ '../pages/Repo'
+    ),
+);
 
 export const Routes: React.FC = () => {
   return (
